048eccb7985c48500856bece457daac53d16c6ba
Andrei Cibotariu <andreicibotariu@outlook.com>
2024-03-20 08:55:04 +0200

    first commit

---
+++ src/test/java/ssvv/example/AppTest.java
@@ -0,0 +1,31 @@
+package ssvv.example;
+
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+
+public class AppTest {
+
+    public void testAddStudent() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+        assert service.saveStudent("10", "nume", 911) == 1;
+        service.deleteStudent("10");
+    }
+}
