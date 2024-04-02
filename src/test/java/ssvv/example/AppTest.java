e7dfebbd639f3cf899803df3446fa0940aa8b37c
Andrei Cibotariu <andreicibotariu@outlook.com>
2024-04-02 20:15:04 +0300

    hehehehehehehe

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -3,6 +3,7 @@ package ssvv.example;
 import domain.Nota;
 import domain.Student;
 import domain.Tema;
+import org.junit.jupiter.api.Test;
 import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
@@ -14,6 +15,7 @@ import validation.Validator;
 
 public class AppTest {
 
+    @Test
     public void testAddStudent() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
@@ -29,6 +31,7 @@ public class AppTest {
         assert service.saveStudent("1001", "a", 911) == 1;
     }
 
+    @Test
     public void testAddAssignment() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
@@ -44,6 +47,7 @@ public class AppTest {
         assert service.saveTema("10", "descriere", 11, 1) == 1;
     }
 
+    @Test
     public void testAddStudentInvalidGroupSmall() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
@@ -59,6 +63,7 @@ public class AppTest {
         assert service.saveStudent("1001", "nume", 110) == 0;
     }
 
+    @Test
     public void testAddStudentOkGroupSmall() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
@@ -74,6 +79,7 @@ public class AppTest {
         assert service.saveStudent("1001", "nume", 111) == 1;
     }
 
+    @Test
     public void testAddStudentInvalidGroupBig() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
@@ -89,6 +95,7 @@ public class AppTest {
         assert service.saveStudent("1001", "nume", 938) == 0;
     }
 
+    @Test
     public void testAddStudentOkGroupBig() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
@@ -104,7 +111,7 @@ public class AppTest {
         assert service.saveStudent("1001", "nume", 937) == 1;
     }
 
-
+    @Test
     public void testAddStudentInvalidName() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
@@ -120,6 +127,7 @@ public class AppTest {
         assert service.saveStudent("1001", "", 911) == 0;
     }
 
+    @Test
     public void testAddStudentOkName() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
@@ -135,6 +143,7 @@ public class AppTest {
         assert service.saveStudent("1001", "a", 911) == 1;
     }
 
+    @Test
     public void testAddStudentWithExistingId() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
@@ -151,6 +160,7 @@ public class AppTest {
         assert service.saveStudent("1", "Andrei", 911) == 0;
     }
 
+    @Test
     public void testAddStudentWithEmptyId() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
